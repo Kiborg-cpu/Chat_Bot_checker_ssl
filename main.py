8ed675e6e89296954df2af80001535c80d671925
UsurpER <stas_gol100x@mail.ru>
2023-02-17 15:28:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import logging
+
+from aiogram import Dispatcher, Bot, types
+from aiogram.utils import executor
+
+import is_link
+
+API_TOKEN = '5618279321:AAHp0qAld0EjJCqmVkdqL2rCG9HibaAjKDY'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.answer("Привет!")
+    await message.answer("Я checker_link_bot!")
+    await message.answer("Могу проверить безопасный ли сайт, который ты хочешь посетить.")
+    await message.answer("Кинь ссылку на сайт, если хочешь в этом убедиться")
+
+
+@dp.message_handler()
+async def my_message_handler(message: types.Message):
+    await message.reply('сейчас проверим')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
